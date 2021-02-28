c3326b181fe7933f7ab421acdc707f91401946d5
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2021-02-28 21:31:54 +0200

    nodejs read html format input names, email & blah

--- NodeJS/ExpressWithParser(lomake)/index.js
+++ NodeJS/ExpressWithParser(lomake)/index.js
@@ -19,8 +19,10 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 const fileUpload = require('express-fileupload')
+//const moment = require('moment')
 
 const members = require('./Members'); //lukaisee olemassa olevan tiedosto polkun
+const logger = require('./middleware/logger') //sama kuin edellinen, vain edessä on kansion nimi
 
 const fs = require('fs');
 
@@ -36,12 +38,7 @@ fs.access(pathFile, (err) => {
 });
 */
 
-app.use(fileUpload({
-    useTempFiles : true,
-    tempFileDir : path.join(__dirname,'tmp'),
-})); //jokin tiedoston uppotaminen
-
-/////
+///////////////
 app.use('/public', express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.urlencoded({express: false}));
 app.use(bodyParser.json());
@@ -67,11 +64,11 @@ app.get('/about', (req,res)=> {
 app.get('/ping', (req,res)=> {
   //res.send("Pong")
   //res.sendFile(path.join(__dirname, pathFile)); //lukee olemassa olevan tiedoston
-  res.sendFile(path.join(__dirname, 'user1.json'));
+  res.sendFile(path.join(__dirname, './tilaus' , 'test.json'));
   console.log("pingpong");
 });
 
-//Lukaisee luoneen json formaatin & nappaa kaikki henkilöt (kaks vaihtoehto toimivat)
+//Lukaisee luoneen json formaatin ja tiedoston & nappaa kaikki henkilöt (kaks vaihtoehto toimivat)
 // 1)::
 /*
 app.get('/api/members', (req,res) => {
@@ -80,28 +77,36 @@ app.get('/api/members', (req,res) => {
   console.log("JSON members");
 });*/
 
-// 2):: localhost:8080/api/memmbers
-app.get('/api/members', (req,res) => {
-  res.json(members)}
-);
+// 2)::
+app.get('/api/members', (req,res) => res.json(members));
 
-//joka kerta kun käyttäjä syöttää polkuu loppuosan eli /done, niin se kuin avaisi toisen sivuston, vain määritetty olemassa oleva linkki. esim html; kun klikkaat sitä sinistä tekstin-osoittavaa polkua niin se avaa toisen url sivuston
-/*
-app.get('/done', (req,res)=> { 
-    res.statusCode = 302;
-  res.setHeader("Location", "https://ExpressWithParseremailandpasslomake.zhaotan18x.repl.co/ping");
-  res.end();
+//Init middleware
+app.use(logger);
+
+//get single members esim. localhost:8080/api/members/2 & jos tiedostossa ei ole sellaista id lukua sitten vaan pelkä tyhjä sivu
+app.get('/api/members/:id', (req, res) => {
+  //res.send(req.params.id);
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if(found) {
+  res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({msg: `No member exist id of: ${req.params.id} ` });
+  }
+
 });
-*/
 
-//////////////////////////////////////////////////////////////////////////////
-//Create random password & muu homma
+
+////////////////////////////////////////////////////////////
+//Create random password / numero & muu homma
+
 var generator = require('generate-password');
 var passWord = generator.generate({
 	length: 5,
 	numbers: true,
 });
 console.log(passWord);
+
 
 //middleware function & että antaa lomake täydentämisen mikälie reittiin näkyä 
 app.use(express.urlencoded({
@@ -113,13 +118,14 @@ const aikaNyt = new Date();
 aikaNyt.toJSON();
 
 var myDate = (aikaNyt.getUTCFullYear()) + "/" + (aikaNyt.getMonth() + 1)+ "/" + (aikaNyt.getUTCDate());
-console.log("Kalenteri:" + myDate);
+console.log("Kalenteri:" + myDate); //pv.kk.vvvv
 
 //var aikaJson = JSON.stringify({'': aikaNyt});
 var aikaJson = JSON.stringify(new Date());
-console.log("nyt:" + aikaJson);
+console.log("nyt:" + aikaJson); //pvkk_HhMinSs
 
 //täs luotu formaatti salasana, mutta fs.writeFileSync kohdassa luodaan se tallentava tiedosto tyyppi. siksi lisätty random salasana funktio/parametri formaatti npm lataus & salasana pituus ja tuleeko numeroa
+
 
 //tulostaa cmd/konsoliin se randomi salasana + määritty sana
 //console.log(password + "asd"); 
@@ -130,25 +136,6 @@ console.log("nyt:" + aikaJson);
 
 app.post('/', (req, res) => {
 
-    if (req.files) 
-    {
-      console.log(req.files)
-      var file = req.files.file //loppu osan file, koska html on määritetty sen id-tunnus mukaa
-      var filename = file.name
-      console.log(filename);
-
-      file.mv('./uploads/' +filename, function(err) {
-        if (err) {
-          res.send(err)
-        }
-        else {
-          console.log("File uploaded");
-          //res.send("file uploaded")
-        }
-      }) //funktio siirrettään tiety kansioon/kohteeseen
-    }
-
-    
     console.log("Lomake ;  " + passWord + JSON.stringify(req.body, null, 2));
     // databse work here, give email & password
     //res.send('successfully '');
@@ -158,8 +145,15 @@ app.post('/', (req, res) => {
   
     //Täs luodaan kuin tallennus tiedosto, että käyttäjä x-aika ja jotakin muuta infoa
     //eli user_milloin(pvKkvv.Aika)luotu_pienitunnus, ja sisäisen lomake kirjoitus ja päiväys
-    fs.writeFileSync('user_' + aikaJson + '_' + passWord + '_.json', dataLomake + "\n\n" + aikaJson );
+    fs.writeFileSync('user_' + aikaJson + '_' + passWord + '_.json', dataLomake + "\n" + "date: " + aikaJson );
     console.log("Saved");
+
+    //appendfile kuin kirjoittaa samaan olemassa olevaan kansioon tai tiedoston peräään kuin kirjoitettu sama asia tai vastaava
+    fs.appendFile(path.join(__dirname, '/tilaus', 'test.json'), ` \n { ${dataLomake} ${myDate} } \n `,
+     err => {
+       if(err) throw err;
+       console.log("upload kirjoituksenne..");
+    });
 
     //res.json({success : true });
 
