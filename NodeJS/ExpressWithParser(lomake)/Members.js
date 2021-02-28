f86fc3ebc99bf1a52b30649566ec2bc05fc72586
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2021-02-28 17:16:06 +0200

    Nodejs path Member.js logger

---
+++ NodeJS/ExpressWithParser(lomake)/Members.js
@@ -0,0 +1,23 @@
+const members = [ 
+  {
+    id: 1,
+    name: 'John Smith',
+    email: 'Smith@gmail.com',
+    country: 'USA'
+
+},
+{
+    id: 2,
+    name: 'Amanda Delta',
+    email: 'AdaW@yahoo.com',
+    country: 'British'
+
+},
+{
+    id: 3,
+    name: 'Cindy Watson',
+    email: 'Cindy@hotmail.com',
+    country: 'Australia'
+}];
+
+module.exports = members;
