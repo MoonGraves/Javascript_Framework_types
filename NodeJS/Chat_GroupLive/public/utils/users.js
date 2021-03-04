9adecd653ed85af550d643bfd5f3155554488ca6
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2021-03-04 21:28:44 +0200

    multiple users if there exist or does

---
+++ NodeJS/Chat_GroupLive/public/utils/users.js
@@ -0,0 +1,36 @@
+const users = [];
+//Kaikki käyttäjät osallistuvat (join) tai poistuvat (leave) group-chattistä
+
+function userJoin(id, username, room ) {
+  const user = { id, username, room};
+
+  users.push(user);
+
+  return user;
+}
+
+//get current user
+function getCurrentUser(id) {
+  return users.find(user => user.id === id);
+}
+
+//user leaves chat
+function userLeave(id) {
+  const index = users.findIndex(user => user.id === id);
+
+  if(index !== -1) {
+    return users.splice(index, 1) [0];
+  }
+}
+
+//get room users 
+function getRoomUsers(room) {
+  return users.filter(user => users.room === room);
+}
+
+module.exports = {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+};
