b37b6e7c7e3405ae7fba46c54ac35a07ee732102
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2021-03-09 20:49:56 +0200

    nimiLista

---
+++ NodeJS/CRASH_GET & POST request/nimiData.js
@@ -0,0 +1,141 @@
+//random json id tunnus ja pälää pälä>> https://www.mockaroo.com/
+const nimiLista = [
+  {
+    "id": 1,
+    "first_name": "Nathalia",
+    "last_name": "Butchard",
+    "email": "nbutchard0@clickbank.net",
+    "gender": "Female",
+    "ip_address": "34.73.42.28",
+    "country": "Bolivia"
+  },
+  {
+    "id": 2,
+    "first_name": "Manon",
+    "last_name": "Nutley",
+    "email": "mnutley1@wordpress.com",
+    "gender": "Non-binary",
+    "ip_address": "41.77.140.237",
+    "country": "Russia"
+  },
+  {
+    "id": 3,
+    "first_name": "Alexi",
+    "last_name": "Lockett",
+    "email": "alockett2@earthlink.net",
+    "gender": "Bigender",
+    "ip_address": "44.208.144.48",
+    "country": "Brazil"
+  },
+  {
+    "id": 4,
+    "first_name": "Vanya",
+    "last_name": "Nassie",
+    "email": "vnassie3@example.com",
+    "gender": "Genderfluid",
+    "ip_address": "20.200.90.52",
+    "country": "China"
+  },
+  {
+    "id": 5,
+    "first_name": "Nigel",
+    "last_name": "Curlis",
+    "email": "ncurlis4@fda.gov",
+    "gender": "Male",
+    "ip_address": "132.137.24.186",
+    "country": "Indonesia"
+  },
+  {
+    "id": 6,
+    "first_name": "Sidoney",
+    "last_name": "Metherell",
+    "email": "smetherell5@phoca.cz",
+    "gender": "Bigender",
+    "ip_address": "182.249.235.205",
+    "country": "China"
+  },
+  {
+    "id": 7,
+    "first_name": "Florrie",
+    "last_name": "Wass",
+    "email": "fwass6@posterous.com",
+    "gender": "Non-binary",
+    "ip_address": "106.26.229.182",
+    "country": "China"
+  },
+  {
+    "id": 8,
+    "first_name": "Simeon",
+    "last_name": "Erickson",
+    "email": "serickson7@wunderground.com",
+    "gender": "Polygender",
+    "ip_address": "24.85.254.221",
+    "country": "China"
+  },
+  {
+    "id": 9,
+    "first_name": "Jolene",
+    "last_name": "Fraczkiewicz",
+    "email": "jfraczkiewicz8@acquirethisname.com",
+    "gender": "Genderfluid",
+    "ip_address": "48.148.90.61",
+    "country": "China"
+  },
+  {
+    "id": 10,
+    "first_name": "Ty",
+    "last_name": "Erni",
+    "email": "terni9@smh.com.au",
+    "gender": "Female",
+    "ip_address": "31.82.78.16",
+    "country": "Democratic Republic of the Congo"
+  },
+  {
+    "id": 11,
+    "first_name": "Rivalee",
+    "last_name": "Whitty",
+    "email": "rwhittya@go.com",
+    "gender": "Genderfluid",
+    "ip_address": "236.72.164.198",
+    "country": "Brazil"
+  },
+  {
+    "id": 12,
+    "first_name": "Katee",
+    "last_name": "Cominoli",
+    "email": "kcominolib@godaddy.com",
+    "gender": "Non-binary",
+    "ip_address": "40.25.120.52",
+    "country": "China"
+  },
+  {
+    "id": 13,
+    "first_name": "Ilysa",
+    "last_name": "Tremain",
+    "email": "itremainc@gmpg.org",
+    "gender": "Non-binary",
+    "ip_address": "90.22.205.83",
+    "country": "Palau"
+  },
+  {
+    "id": 14,
+    "first_name": "Torr",
+    "last_name": "Giriardelli",
+    "email": "tgiriardellid@typepad.com",
+    "gender": "Genderqueer",
+    "ip_address": "150.70.247.242",
+    "country": "Indonesia"
+  },
+  {
+    "id": 15,
+    "first_name": "Alec",
+    "last_name": "Goodburn",
+    "email": "agoodburne@google.ru",
+    "gender": "Genderfluid",
+    "ip_address": "64.19.56.77",
+    "country": "Uzbekistan"
+  }
+]
+
+
+module.exports = nimiLista; 
