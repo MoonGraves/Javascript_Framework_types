fb435f0ba51e6bdd7ec4cd1992fdfd6c4550ad04
zhaotan18x <zhao-96@hotmail.com>
2021-06-26 21:14:55 +0300

    nodejs files

---
+++ NodeJS Swagger/Auto-swagger-firstApp/index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const app = express();
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+// tekijän tiedot, kuin tämän sivun (sovelluksen) ominaisuudet, versiot, yhteistiedot ja jne. 
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0', //vain 2.0 tai 3.0 versio
+      info: 
+      {
+        title: "Library API PäläPälä",
+        version: '1.2.3', //versio, se näkyy polussa /api-docs
+        description:'Something programming about API software',
+        //yhteistiedot
+        contact:{
+                name:'PäläPälä Lounas',
+                url:'https://blahblah.com',
+                email:'blah@gmail.com',
+        },
+        license: {
+            name: 'Apache 2.0',
+            url: 'http://www.apache.org/licenses/LICENSE-2.0.html',
+        },
+      },
+    },
+    apis: ["index.js"], //tämä pääsovelluksen nimi
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+console.log(swaggerDocs);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs)); //polku käytettään tätä aktivoituneena
+
+//Home page
+app.get('/', (req, res) => { 
+    //console.log('[Test]');
+
+    res.send("Moikka")
+});
+
+
+//Vastaanottaa olemassa oleva aihe tai esine, esim määärityn id, aihe ja jne.
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     description: Get all books
+ *     responses:
+ *       200:
+ *         description: Success
+ * 
+ */
+//oletus tyhjä sivusto polku (books), sivustossa näkyy json array tiedot
+app.get('/books', (req, res) => {
+  res.send([
+    {
+      id: 10,
+      title: "Mickey Mouse",
+      pages: 500,
+    },
+    {
+      id: 20,
+      title: "Donald Duck",
+      pages: 123,
+    }
+    //ja jne, jatkuu voi lisätä useammankin, älä unohda lisätä pilkkua!!
+
+  ])
+});
+
+//Kirjan tai aihe julkaisu
+/**
+ * @swagger
+ * /books:
+ *   post:
+ *     description: Get all books
+ *     parameters:
+ *      - name: title
+ *        description: title of the book
+ *        in: formData
+ *        required: true
+ *        type: string
+ *      - pages: title
+ *        type: int
+ *     responses:
+ *       201:
+ *         description: Created
+ */
+app.post('/books', (req, res) => {
+  res.status(201).send();
+});
+
+app.listen(8080, () => console.log("listening on 8080"));
