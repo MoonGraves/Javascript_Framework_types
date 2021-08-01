bd4c90c20baa00bb4bce3586d584cad3b641bc55
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2021-08-01 14:34:45 +0300

    Create app2.js

---
+++ NodeJS/JSON_readWriteUpdateFunction/app2.js
@@ -0,0 +1,104 @@
+/*Tämä liittyy edellisen app.js , mutta vähä selkeämmin erottaa toisesta sovelluksesta ja toiminassa
+
+Tänne tulee, että kirjoitettaan json tiedoston sisälle jotakin muutosta tai lisätään jotaki
+
+READ, WRITE AND UPDATE JSON file:::
+*/
+
+//Tässä tapahtuu ensin kuin lisättäisi lisä jotakin lukua jollekin asialle vaikka tilaus luku, tai muu vastaava "value" & 
+//toki on muita tapoja, että esim muutettaisi tekijän objektia esim. nimi.
+
+const fs = require("fs");
+
+//function read json file path and some settings
+ function jsonReader(filePath, cb) 
+{
+  fs.readFile(filePath, (err, fileData) => {
+    if (err) {
+      return cb && cb(err);
+    }
+    try 
+    {
+      const object = JSON.parse(fileData);
+      return cb && cb(null, object);
+    } 
+    
+    catch (err) 
+    {
+      return cb && cb(err);
+    }
+  });
+}
+
+//user write json file where will be updates and something will happens by wizard
+jsonReader("./customer.json", (err, customer) => {
+  if (err) {
+    console.log("Error reading file:", err);
+    return;
+  }
+  // increase customer order count by 1
+  customer.order_count += 1;
+  fs.writeFile("./customer.json", JSON.stringify(customer, null, 2), err => {
+    if (err) console.log("Error writing file:", err);
+  });
+});
+
+/* json file::
+
+>>>> BEFORE <<<<
+{
+  "name": "John Doe",
+  "id": 12345,
+  "order_count": 87,
+  "address": "Admin Some Drive",
+  "city": "Texas"
+}
+
+>>>> AFTER <<<<
+the order_count one value added
+{
+  "name": "John Doe",
+  "id": 12345,
+  "order_count": 88, 
+  "address": "Admin Some Drive",
+  "city": "Texas"
+}
+*/
+
+//TODO:: another example if change city or something by same action :::
+//change object or give a new name, and new order
+
+jsonReader("./customer.json", (err, customer) => {
+  if (err) {
+    console.log("Error reading file:", err);
+    return;
+  }
+  // increase customer order count by 1 & or change object or give a new name
+  customer.city = "Miami";
+  customer.name = "Billy Watson";
+  fs.writeFile("./customer.json", JSON.stringify(customer, null, 2), err => {
+    if (err) console.log("Error writing file:", err);
+  });
+  
+});
+
+/*
+>>>> BEFORE <<<<
+{
+  "name": "John Doe",
+  "id": 12345,
+  "order_count": 87,
+  "address": "Admin Some Drive",
+  "city": "Texas"
+}
+
+>>>> AFTER <<<<
+the order_count one value added
+{
+  "name": "Billy Watson",
+  "id": 12345,
+  "order_count": 88,
+  "address": "Admin Some Drive",
+  "city": "Miami"
+}
+*/
