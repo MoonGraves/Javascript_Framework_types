dde411289d2a25be0127d6152cbcefc975ae9d04
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2021-08-02 13:32:57 +0300

    Create users.js

---
+++ NodeJS/RoutesPath_readJsonFile/routes/users.js
@@ -0,0 +1,19 @@
+const userRoutes = (app, fs) => {
+  // variables & analyze the json file and data
+  const dataPath = './data/users.json';
+
+  // READ folder data and there is the json file & readfile function
+  app.get('/users', (req, res) => {
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      res.send(JSON.parse(data, null, 2));
+    });
+
+  });
+
+};
+
+module.exports = userRoutes;
