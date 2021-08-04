cf4723e229719bf8c3fa95674d77e13658edfca1
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2021-08-04 12:07:39 +0300

    Create index.js

---
+++ NodeJS/RoutesPath_readJSONFile_withExpressEJs/index.js
@@ -0,0 +1,31 @@
+//TÄMÄ ON PÄÄSOVELLUS & SERVER
+// load up the express framework and body-parser helper
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const app = express();
+
+//loading node's file systems
+const fs = require('fs');
+
+//Aktivoidaan tämä ejs html-editoitava pohja setti
+const ejs = require('ejs');
+//EJS Engine & ensimmäinen sivu
+app.set('view engine', 'ejs');
+app.use(cookieParser());
+
+//lukaisee/käsittelee olemassa olevan kansion ja sisäisen tiedoston json muodon & ja ejs kohdassa pitää määrittää sama tekijä, että tämä on kuin kohde osoite
+app.locals.classUsers = require('./data/users.json');
+
+//configure our express instance with some body-parser settings
+//including handling JSON datas
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//consider routes path folder/files, it will set to default home page
+const routes = require('./routes/routes.js')(app, fs);
+
+//set server running and port number
+const server = app.listen(8080, () => {
+  console.log('Server is running %s...', server.address().port);
+});
