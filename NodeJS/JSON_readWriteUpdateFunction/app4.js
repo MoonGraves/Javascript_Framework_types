7bcd6c89cbfefb63e4d82fad1aea1de0002bdd54
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2021-11-18 10:55:56 +0200

    Create app4.js

---
+++ NodeJS/JSON_readWriteUpdateFunction/app4.js
@@ -0,0 +1,98 @@
+//Tässä muutama esimerkki js funktiosta, että kuinka lukaisee json tiedoston ja output, mitä ne tulostaa siinä komentolähteessä
+//Osassa saattaa olla melko sama methodi, mutta toiminnaltaan oikea suunta & sekä erillisenä määritettään objekti tekijä, että mitä tiettyä halutaan kuin lukaista ja nähdä komentolähteessä 
+
+/* JSON read, write & update notes::
+JSON.stringify tarkoittaa: methodin muuntamista javascript:ssä, että objektin tai numeron json merkkijonoksi ja korvaa valinnaisesti arvoa
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
+JSON.parse tarkoittaa: menetelmä jäsentää JSOn-merkijonoa ja muodostaa merkkijonon kuvaamista javascript arvoa tai objektia. Valinnaisen elvytystoiminnossa voi tarjota suorittamia muunnosta tuloksen olevia objektille ennen sen palautumista.
+*/
+
+//TODO: lisäksi tämä vähä toistuu edellisen sovellukseen/harjoitukseen app1, 2 tai 3:seen, mutta tässä on vähä enemmän syvemmin/vaikeampi JSON tiedosto lukeminen
+
+//json tiedosto
+const appSoft = {
+  "creater": [{
+  "user1" : {
+    "id": 1,
+    "username": "cdegoey0",
+    "file": "MaurisUllamcorper.mp3",
+    "App": "Treeflex"
+  }, 
+
+  "user2" :{
+    "id": 2,
+    "username": "akleinbaum1",
+    "file": "FelisUtAt.xls",
+    "App": "Flowdesk"
+  }, 
+
+  "user3": {
+    "id": 3,
+    "username": "lbrighty2",
+    "file": "RhoncusAliquam.mp3",
+    "App": "Tempsoft"
+  }, 
+
+  "user4":{
+    "id": 4,
+    "username": "crotlauf3",
+    "file": "NullaUltrices.xls",
+    "App": "Bigtax"
+  }, 
+
+  "user5":{
+    "id": 5,
+    "username": "vhawford4",
+    "file": "CongueEtiamJusto.avi",
+    "App": "Konklux"
+  }, 
+
+  "user6":{
+    "id": 6,
+    "username": "jblackeby5",
+    "file": "EuMassa.avi",
+    "App": "Transcof"
+  }, 
+
+  "user7":{
+    "id": 7,
+    "username": "smarcinkowski6",
+    "file": "NullaDapibus.avi",
+    "App": "Ventosanzap"
+  }, 
+
+  "user8":{
+    "id": 8,
+    "username": "clum7",
+    "file": "PharetraMagna.xls",
+    "App": "Ximokowe"
+  }, 
+
+  "user9":{
+    "id": 9,
+    "username": "fportingale8",
+    "file": "Suscipit.avi",
+    "App": "Fix San"
+  },
+
+  "user10":{
+    "id": 10,
+    "username": "keddisforth9",
+    "file": "LoremVitaeMattis.ppt",
+    "App": "Wrapsafe"
+  }}
+]}
+
+
+//TODO:: 1 - esim
+//tulostaa koko json sisäisen datat & että user x tyypit
+const appJSON = JSON.stringify(appSoft);
+const appJSONParse = JSON.parse(appJSON);
+console.log(appJSONParse.creater); //tulostaa kaikki
+
+
+
+
+
+
+
